f779ac713d6ebb51941b423ad1619e721ec4cd0f
Scott Jansen <scott.jansen731@outlook.com>
2015-03-21 21:36:47 -0500

    added php intro page

---
+++ emailCritique.php
@@ -0,0 +1,27 @@
+﻿
+<?php
+// This receives input from Critiques.html and sends an email off to Mary with the information.
+
+$nameInput = $_POST['nameInput'];
+$fromEmail = $_POST['emailInput'];
+$emailBody = $_POST['comments'];
+$emailAttachment = $_POST['upFile'];
+$emailSubject = 'Critique Request from ' . $nameInput;
+$toEmail = 'marymjansen@msn.com';
+
+$searchAt = strpos($fromEmail, '@');
+if ($searchAt === false) {
+   echo 'Please go back and enter a valid email address';
+   } elseif {
+       $emailParts = explode('@',$fromEmail);
+       $searchDot = strpos($emailParts[1], '.');
+       if ($searchDot === false) {
+           echo 'Please go back and enter a valid email address';
+       } else {
+           echo 'Your request has been submitted.';
+   }
+
+   //I still need to learn how to send an email from php. The tutorials did not cover that.
+
+?>
+
